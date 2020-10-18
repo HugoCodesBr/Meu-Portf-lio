6206ea3875ba3defb18a3703c8c5ca3f2fd1eb06
HugoCodesBr <herbtyh@gmail.com>
2020-10-18 13:01:50 -0300

    1°  Versao Online

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var e = document.getElementById('catalogo');
+e.onmouseover = function() {
+  document.getElementById('popup').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('popup').style.display = 'none';
+}
